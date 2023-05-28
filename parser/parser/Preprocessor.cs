46bc0c392bf3365dcdebff3defde7512f9b322e0
elliotmb <elliotbuckingham14@gmail.com>
2023-05-28 18:18:55 +0100

    changing tokens to statements

--- parser/parser/Preprocessor.cs
+++ parser/parser/Preprocessor.cs
@@ -51,6 +51,6 @@ namespace parser
             return noWhiteSpace;
         }
 
-
+        
     }
 }
---
+++ parser/parser/Token.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parser
+{
+    abstract class Token
+    {
+        public abstract Token Visit(TokenVisitor tv);
+
+        public class Literal : Token
+        {
+
+        }
+
+        public class Operator : Token
+        {
+
+        }
+
+        public class Bracket : Token
+        {
+
+        }
+    }
+}
